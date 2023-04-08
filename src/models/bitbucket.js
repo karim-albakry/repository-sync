f21338a14a5a97ff3e59e5bbdf170a28b69e8931
karim.albakry@gmail.com <karim.albakry@gmail.com>
2023-04-09 00:53:56 +0200

    refactor: BitbucketClient class to improve code readability and remove unnecessary caching functionality. The listRepositories method has been simplified and now uses a recursive function getValues to retrieve all repositories from the Bitbucket API. The Cache dependency has been removed as caching is no longer needed. The code has been updated with JSDoc comments for improved documentation.

--- src/models/bitbucket.js
+++ src/models/bitbucket.js
@@ -1,8 +1,16 @@
 const { Bitbucket } = require("bitbucket");
-const Cache = require("../utils/cache");
-const { isEmpty } = require("../utils/validator");
 
+/**
+ * A class for interacting with the Bitbucket API.
+ * @class BitbucketClient
+ */
 class BitbucketClient {
+  /**
+   * Creates a new instance of the BitbucketClient class.
+   * @param {string} bitbucketUser - The Bitbucket username to use for authentication.
+   * @param {string} bitbucketPass - The Bitbucket password to use for authentication.
+   * @param {string} bitbucketWorkspaceName - The name of the Bitbucket workspace to retrieve repositories from.
+   */
   constructor(bitbucketUser, bitbucketPass, bitbucketWorkspaceName) {
     const clientOptions = {
       baseUrl: "https://api.bitbucket.org/2.0",
@@ -13,47 +21,56 @@ class BitbucketClient {
     };
     this.bitbucket = new Bitbucket(clientOptions);
     this.workspaceName = bitbucketWorkspaceName;
-    this.cache = new Cache();
+
+    /**
+     * Retrieves a list of repositories from the Bitbucket API.
+     * @async
+     * @function
+     * @param {number} pageNumber - The page number of the repositories to retrieve.
+     * @param {string} query - The query to use when retrieving the repositories.
+     * @returns {Promise<Array>} An array of repository objects.
+     * @memberof BitbucketClient
+     * @inner
+     */
+    this.getValues = async (pageNumber, query) => {
+      const { values, next } = await this.bitbucket.repositories
+        .list({
+          workspace: this.workspaceName,
+          page: pageNumber,
+          q: query,
+        })
+        .then((res) => res.data);
+
+      if (next) {
+        return [...values, ...(await this.getValues(pageNumber + 1, query))];
+      }
+      return values;
+    };
   }
 
   /**
-   * Lists all repositories in the specified Bitbucket workspace.
-   *
+   * Retrieves a list of repositories for the configured Bitbucket workspace.
    * @async
-   * @param {Object} [options] - Optional options to enable caching.
-   * @param {boolean} [options.enableCache=false] - Whether to enable caching of the repository list.
-   * @returns {Promise<Array>} - A Promise that resolves to an array of repository objects.
-   * @throws {Error} - If an error occurs during the repository listing, an error is thrown with a descriptive message.
+   * @function
+   * @param {Object} [options={}] - An options object to configure the repository list.
+   * @param {string} [options.query=""] - The query to use when retrieving the repositories.
+   * @param {Array} [options.exclude=[]] - An array of repository slugs to exclude from the list.
+   * @returns {Promise<Array>} An array of repository objects.
+   * @throws {Error} If the options parameter is not an object.
+   * @memberof BitbucketClient
    */
   async listRepositories(options = {}) {
     if (typeof options !== "object") {
       throw new Error("Options must be an object");
     }
 
-    const { enableCache = false, query = "", exclude = [] } = options;
+    const { query = "", exclude = [] } = options;
 
-    let pageNumber = 1;
-    let nextPage = null;
-    let repos = [];
+    const repos = await this.getValues(1, query, {
+      bitbucket: this.bitbucket,
+      workspaceName: this.workspaceName,
+    });
 
-    try {
-      while (nextPage || pageNumber === 1) {
-        // TODO: Use async iterator instead.
-        const { values, page, next } = await this.bitbucket.repositories
-          .list({ workspace: this.workspaceName, page: pageNumber, q: query })
-          .then((res) => res.data);
-
-        pageNumber = page + 1;
-        nextPage = next;
-        repos = [...repos, ...values];
-      }
-    } catch (err) {
-      throw new Error(`Failed to list repositories: ${err.message}`);
-    }
-
-    if (enableCache) {
-      this.cache.set("repos_list", repos);
-    }
     return repos.filter(({ slug }) => !exclude.includes(slug));
   }
 }
